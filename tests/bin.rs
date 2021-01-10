62555ff2eca745c4266fc3984074e5a174ea1d36
philipahlberg <philipahlberg@gmail.com>
2021-01-10 23:44:27 +0100

    Run integration tests with arguments instead of stdin

--- tests/bin.rs
+++ tests/bin.rs
@@ -1,22 +1,15 @@
-use std::io::Write;
 use std::process::{Command, Stdio};
 
 const BIN: &'static str = env!("CARGO_BIN_EXE_wasmfmt");
 
-fn run(input: &str) -> Result<String, String> {
-    let mut process = Command::new(BIN)
+fn wasmfmt(args: &[&str]) -> Result<String, String> {
+    let process = Command::new(BIN)
+        .args(args)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to spawn process");
 
-    let stdin = process
-        .stdin
-        .as_mut()
-        .expect("failed to get stdin handle from process");
-    stdin
-        .write_all(input.as_bytes())
-        .expect("failed to write to process' stdin");
     let output = process
         .wait_with_output()
         .expect("failed to get process output");
@@ -33,32 +26,28 @@ fn run(input: &str) -> Result<String, String> {
 
 #[test]
 fn add_desugar() {
-    let input = include_str!("data/input/add_desugar.wat");
     let expected = include_str!("data/output/add_desugar.wat");
-    let actual = run(input).expect("failed to format add_desugar.wat");
+    let actual = wasmfmt(&["tests/data/input/add_desugar.wat"]).expect("failed to format add_desugar.wat");
     assert_eq!(actual, expected);
 }
 
 #[test]
 fn add_sugar() {
-    let input = include_str!("data/input/add_sugar.wat");
     let expected = include_str!("data/output/add_sugar.wat");
-    let actual = run(input).expect("failed to format add_sugar.wat");
+    let actual = wasmfmt(&["tests/data/input/add_desugar.wat"]).expect("failed to format add_sugar.wat");
     assert_eq!(actual, expected);
 }
 
 #[test]
 fn fac_desugar() {
-    let input = include_str!("data/input/fac_desugar.wat");
     let expected = include_str!("data/output/fac_desugar.wat");
-    let actual = run(input).expect("failed to format fac_desugar.wat");
+    let actual = wasmfmt(&["tests/data/input/fac_desugar.wat"]).expect("failed to format fac_desugar.wat");
     assert_eq!(actual, expected);
 }
 
 #[test]
 fn fac_sugar() {
-    let input = include_str!("data/input/fac_sugar.wat");
     let expected = include_str!("data/output/fac_sugar.wat");
-    let actual = run(input).expect("failed to format fac_sugar.wat");
+    let actual = wasmfmt(&["tests/data/input/fac_sugar.wat"]).expect("failed to format fac_sugar.wat");
     assert_eq!(actual, expected);
 }
