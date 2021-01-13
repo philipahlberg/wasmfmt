e83e8b0c810ef78fd1b97d7093bdb8ff26a53127
Philip Ahlberg <philipahlberg@gmail.com>
2021-01-14 00:59:53 +0100

    Add stdin test (#39)

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs;
-use std::io::Error;
+use std::io::{Error, Write};
 use std::process::{Command, Stdio};
 
 const BIN: &'static str = env!("CARGO_BIN_EXE_wasmfmt");
@@ -12,6 +12,38 @@ fn wasmfmt(args: &[&str]) -> Result<String, String> {
         .stdout(Stdio::piped())
         .spawn()
         .expect("failed to spawn process");
+
+    let output = process
+        .wait_with_output()
+        .expect("failed to get process output");
+
+    let stdout = String::from_utf8(output.stdout).expect("stdout");
+    let stderr = String::from_utf8(output.stderr).expect("stderr");
+
+    if !stderr.is_empty() {
+        Err(stderr)
+    } else {
+        Ok(stdout)
+    }
+}
+
+fn wasmfmt_stdin(input: &str) -> Result<String, String> {
+    let mut process = Command::new(BIN)
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn process");
+
+    let stdin = process
+        .stdin
+        .as_mut()
+        .expect("failed to get stdin handle from process");
+
+    stdin
+        .write_all(input.as_bytes())
+        .expect("failed to write to stdin");
+
+    stdin.flush().unwrap();
 
     let output = process
         .wait_with_output()
@@ -85,7 +117,7 @@ fn writes_to_output_file() -> Result<(), Error> {
     let formatted = include_str!("data/output/add_sugar.wat");
     let tmp = env::temp_dir();
     let out = tmp.join("out.wat");
-    wasmfmt(&[
+    let result = wasmfmt(&[
         "tests/data/input/add_sugar.wat",
         "--output",
         out.to_str().unwrap(),
@@ -93,6 +125,16 @@ fn writes_to_output_file() -> Result<(), Error> {
     .expect("failed to format add_sugar.wat");
     let content = fs::read_to_string(&out)?;
     assert_eq!(content, formatted);
+    assert_eq!(result, String::new());
     fs::remove_file(out)?;
+    Ok(())
+}
+
+#[test]
+fn reads_from_stdin_if_no_file_is_provided() -> Result<(), Error> {
+    let source = include_str!("data/input/add_sugar.wat");
+    let formatted = include_str!("data/output/add_sugar.wat");
+    let result = wasmfmt_stdin(source).expect("failed to format add_sugar.wat");
+    assert_eq!(result, formatted);
     Ok(())
 }
