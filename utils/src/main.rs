a9d206c0708c29aa3a253674d539b742db83940c
philipahlberg <philipahlberg@gmail.com>
2021-01-30 12:43:41 +0100

    Add `utils` crate

---
+++ utils/src/main.rs
@@ -0,0 +1,29 @@
+use std::{fs, path::PathBuf};
+use structopt::StructOpt;
+
+use wast::{Wat, Error, ModuleKind, parser::{self, ParseBuffer}};
+
+#[derive(StructOpt)]
+pub enum Utils {
+    Parse {
+        #[structopt(parse(from_os_str))]
+        file: PathBuf,
+    }
+}
+
+fn main() -> Result<(), Error> {
+    match Utils::from_args() {
+        Utils::Parse { file } => {
+            let input = fs::read_to_string(file.as_path())
+                .expect("file");
+            let buffer = ParseBuffer::new(&input).expect("ParseBuffer");
+            let wat = parser::parse::<Wat>(&buffer).expect("parse");
+            let fields = match wat.module.kind {
+                ModuleKind::Text(fields) => fields,
+                ModuleKind::Binary(..) => todo!(),
+            };
+            println!("{:#?}", fields);
+        }
+    }
+    Ok(())
+}
