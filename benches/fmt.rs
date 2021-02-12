1d10ef3270a900b748f16f9be56e80b168a55faa
philipahlberg <philipahlberg@gmail.com>
2021-02-13 00:09:58 +0100

    Fix benchmark

--- benches/fmt.rs
+++ benches/fmt.rs
@@ -1,10 +1,10 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use wasmfmt::{fmt, Options};
+use wasmfmt::{wat, Options};
 
 fn fmt_i32(c: &mut Criterion) {
     let id = BenchmarkId::new("fmt", "i32");
     let input = include_str!("../tests/data/input/i32.wat");
-    c.bench_with_input(id, &input, |b, i| b.iter(|| fmt(i, Options::default())));
+    c.bench_with_input(id, &input, |b, i| b.iter(|| wat::fmt(i, Options::default())));
 }
 
 criterion_group! {
